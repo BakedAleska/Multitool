6ab115b0160cb9538224a60589ea34bf49149bcb
BakedAleska <al3velazquez2007@gmail.com>
2026-08-04 16:05:21 -0400

    chore: bump version to 0.1.1-beta

--- toolblox/version.py
+++ toolblox/version.py
@@ -5,4 +5,4 @@ each release. toolblox.updater compares this against GitHub's latest
 release tag to decide whether an update is available.
 """
 
-APP_VERSION = "0.1.0-beta"
+APP_VERSION = "0.1.1-beta"
